756f32eaa4fc8f463a74c8b93540b36a569655cb
Laith <62894505+lth17elm@users.noreply.github.com>
2020-10-31 19:10:38 +0100

    update drop 'physique'

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,7 +28,7 @@
                     </li>
                     <li><a href="#">Physique <i class="fas fa-sort-down"></i></a>
                         <ul class="drop">
-                            <li><router-link to="/physiquememo">PhysiqueMemo</router-link></li>
+                            <li><router-link to="/physiquememo">Memo Physique</router-link></li>
                             <li><a href="#">Physique 2</a></li>
                           </ul>
                     </li>
