09c4399cbf2cba648f6e2542d3ffb2e993e2a6e5
Laith <62894505+lth17elm@users.noreply.github.com>
2020-11-04 22:58:11 +0100

    create cryptocurrency page

---
+++ client/src/views/Crypto.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mt-5">
+    <main>
+      <h1 id="theme">Finance</h1>
+      <div class="main">
+        <article>
+          <h1>Cryptmonnaie</h1>
+          <h2>Top 3 des cryptomonnaies dominantes</h2>
+          <ul>
+          <li
+            v-for="crypto in cryptomonnaies"
+            :key="crypto"
+          >
+          {{crypto}}
+          </li>
+          </ul>
+        </article>
+      </div>
+    </main>
+  <foot></foot>
+  </div>
+</template>
+
+<script>
+import Foot from '../components/foot.vue'
+export default {
+  name: 'Crypto',
+  data () {
+    return {
+      cryptomonnaies: []
+    }
+  },
+  methods: {
+    getCrytpo () {
+      fetch('/api/v1/langagesinfo')
+        .then(res => res.json())
+        .then(data => { this.cryptomonnaies = data })
+    }
+  },
+
+  created () {
+    this.getCrypto()
+  },
+
+  components: {
+    foot: Foot
+  }
+}
+</script>
