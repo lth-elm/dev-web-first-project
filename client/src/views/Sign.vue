58a751780e39b235f26f0938571c4de0ce643bbc
Laith <62894505+lth17elm@users.noreply.github.com>
2020-11-08 00:08:02 +0100

    added css style

--- client/src/views/Sign.vue
+++ client/src/views/Sign.vue
@@ -1,45 +1,44 @@
 <template>
-    <div class="container mt-5">
-        <main>
-            <div class="main">
-                <article>
-                    <h1>Sign In/UP</h1>
-                <form>
-                    <div class="form-group">
-                        <label for="prenom">Prenom</label>
-                        <input v-model="prenom" type="text" id="prenom" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="nom">Nom</label>
-                        <input v-model="nom" type="text" id="nom" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="mail">Email</label>
-                        <input v-model="mail" type="email" id="mail" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="cle">mot de passe</label>
-                        <input type="password" id="cle" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="txt">description rapide</label>
-                        <textarea v-model="txt" class="form-control" id="txt"></textarea>
-                    </div>
-                </form>
-                <h2>Résultat</h2>
-                <div class="card p-3">
-                    <p>Prénom : {{prenom}}</p>
-                    <p>Nom : {{nom}}</p>
-                    <p>Email : {{mail}}</p>
-                    <p>description : {{txt}}</p>
+    <main>
+            <article>
+                <h1>Sign In/UP</h1>
+            <form>
+                <div class="form-group">
+                    <label for="prenom">Prénom</label>
+                    <input v-model="prenom" v-on:input="refaire" type="text" id="prenom" class="form-control" placeholder="Prénom">
                 </div>
-                </article>
+
+                <div class="form-group">
+                    <label for="nom">Nom</label>
+                    <input v-model="nom" v-on:input="refaire" type="text" id="nom" class="form-control" placeholder="Nom">
+                </div>
+
+                <div class="form-group">
+                    <label for="mail">Email</label>
+                    <input v-model="mail" v-on:input="refaire" type="email" id="mail" class="form-control" placeholder="Mail">
+                </div>
+                <div class="form-group">
+                    <label for="cle">Mot de passe</label>
+                    <input type="password" v-on:input="refaire" id="cle" class="form-control" placeholder="Mot de Passe">
+                </div>
+                <div class="form-group">
+                    <label for="txt">Description rapide</label>
+                    <textarea v-model="txt" v-on:input="refaire" class="form-control" id="txt" placeholder="Description" maxlength="50"></textarea>
+                </div>
+            </form>
+            <div v-if="infoSubmit" class="resultat">
+            <h2>Résultat</h2>
+            <div class="Tab">
+                <p>Prénom : {{prenom}}</p>
+                <p>Nom : {{nom}}</p>
+                <p>Email : {{mail}}</p>
+                <p>Description : {{txt}}</p>
             </div>
-        </main>
+            </div>
+            </article>
+            <button v-if="CheckButtom" v-on:click="envoiForm" class="btn">Envoyer</button>
+    </main>
     <foot></foot>
-    </div>
 </template>
 <script>
 export default {
@@ -49,8 +48,60 @@ export default {
       prenom: '',
       nom: '',
       mail: '',
-      txt: ''
+      txt: '',
+      infoSubmit: false,
+      CheckButtom: true
+    }
+  },
+  methods: {
+    envoiForm: function () {
+      this.infoSubmit = true
+      this.CheckButtom = false
+    },
+    refaire: function () {
+      this.infoSubmit = false
+      this.CheckButtom = true
     }
   }
 }
 </script>
+
+<style scoped>
+.form-group {
+    display:inline-block;
+    width: 60%;
+    justify-content: center;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+}
+.form-group >label{
+    margin-bottom: .5rem;
+    font-size: larger;
+}
+.form-control {
+    display: block;
+    width: 100%;
+    height: calc(1.5em + .75rem + 2px);
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25rem;
+}
+.resultat {
+    margin: 3em 0 0 0;
+    display: inline-block;
+    width: 60%;
+    height: 300px;
+    font-size: larger;
+    border: solid grey;
+}
+.Tab {
+    padding: 0 0 0 1em;
+    color:rgb(102, 102, 102);
+}
+</style>
