d308a092b3ceb2372bc9f3d116541108cd20f33b
Laith <62894505+lth17elm@users.noreply.github.com>
2020-11-08 00:12:09 +0100

    update text

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,7 +5,7 @@
       <div class="main">
           <article>
               <h1>Présentation projet</h1>
-              <p style="color: red;"><em>Seul les pages C#, Brainstorming, Memo Physique et Sign In ont été réalisés.</em></p>
+              <p style="color: red;"><em>Pages "terminés" : Home, C#, Brainstorming, Memo Physique, Cryptomonnaie et Sign In.</em></p>
               <p>>>> Faire une présentation du site et des différentes fonctionnalitées ajoutées.</p>
               <p>Vivamus sed libero nec mauris pulvinar facilisis ut non sem. Quisque mollis ullamcorper diam vel faucibus. Vestibulum sollicitudin facilisis feugiat. Nulla euismod sodales hendrerit. Donec quis orci arcu. Vivamus fermentum magna a erat ullamcorper dignissim pretium nunc aliquam. Aenean pulvinar condimentum enim a dignissim. Vivamus sit amet lectus at ante adipiscing adipiscing eget vitae felis. In at fringilla est. Cras id velit ut magna rutrum commodo. Etiam ut scelerisque purus. Duis risus elit, venenatis vel rutrum in, imperdiet in quam. Sed vestibulum, libero ut bibendum consectetur, eros ipsum ultrices nisl, in rutrum diam augue non tortor. Fusce nec massa et risus dapibus aliquam vitae nec diam.</p>
               <p>Phasellus ligula massa, congue ac vulputate non, dignissim at augue. Sed auctor fringilla quam quis porttitor. Praesent vitae dignissim magna. Pellentesque quis sem purus, vel elementum mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas consectetur euismod urna. In hac habitasse platea dictumst. Quisque tincidunt porttitor vestibulum. Ut iaculis, lacus at molestie lacinia, ipsum mi adipiscing ligula, vel mollis sem risus eu lectus. Nunc elit quam, rutrum ut dignissim sit amet, egestas at sem.</p>
