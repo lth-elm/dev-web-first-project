dd1575ffbc8ab3ba60d268641906727bc27441c8
Laith <62894505+lth17elm@users.noreply.github.com>
2020-11-15 13:29:54 +0100

    verifytoken path added

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const langagesinfoRoutes = require('./langagesinfo-routes.js')
 const currenciesRoutes = require('./currencies-routes.js')
+const currenciesVerifyTokenRoutes = require('./currencies-verifytoken-routes.js')
 const authRoutes = require('./auth-routes.js')
 
 const verifyToken = require('../middlewares/verify-token.js')
@@ -11,6 +12,7 @@ const router = new express.Router()
 
 router.use('/langagesinfo', langagesinfoRoutes)
 router.use('/currencies', currenciesRoutes)
+router.use('/verifytoken/currencies', verifyToken, currenciesVerifyTokenRoutes)
 router.use('/auth', authRoutes)
 
 module.exports = router
