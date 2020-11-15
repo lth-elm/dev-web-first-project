d6a5d3b378fd0bcf8b28af49982cf315e0a52d0c
Laith <62894505+lth17elm@users.noreply.github.com>
2020-11-15 16:11:01 +0100

    text formatting

--- client/src/components/Texte1.vue
+++ client/src/components/Texte1.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="txt1">
         <h3>Devinette n°1</h3>
-        <p>Naissance : 10 juillet 1856, Smijan (Autriche).</p>
-        <p>Décès : 7 janvier 1943, New York.</p>
-        <p>Activités : Inventeur, ingénieur généraliste.</p>
-        <p>Invention :  moteur électrique asynchrone, l’alternateur polypha,
+        <p><b>Naissance</b> : 10 juillet 1856, Smijan (Autriche).</p>
+        <p><b>Décès</b> : 7 janvier 1943, New York.</p>
+        <p><b>Activités</b> : Inventeur, ingénieur généraliste.</p>
+        <p><b>Invention</b> :  moteur électrique asynchrone, l’alternateur polypha,
                         le montage triphasé en étoile, la commutatrice.</p>
         <br>
         <p>Le 6 novembre 1915, la une du New York Times annonce les prochains lauréats du prix Nobel de physique.</p>
--- client/src/components/Texte2.vue
+++ client/src/components/Texte2.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <h3>Devinette n°2</h3>
-      <p>Naissance : 30 avril 1777, Brunwick.</p>
-      <p>Décès : 23 février 1855, Göttingen.</p>
-      <p>Activités : Travaux en mathématiques et en physiques.</p>
-      <p>Prix : Médaille Copley, Prix Lalande, Ordre bavarois de Maximilien pour la science et l'art.</p>
+      <p><b>Naissance</b> : 30 avril 1777, Brunwick.</p>
+      <p><b>Décès</b> : 23 février 1855, Göttingen.</p>
+      <p><b>Activités</b> : Travaux en mathématiques et en physiques.</p>
+      <p><b>Prix</b> : Médaille Copley, Prix Lalande, Ordre bavarois de Maximilien pour la science et l'art.</p>
       <br>
       <p>1799 — (la) Demonstratio nova theorematis omnem functionem algebraicam rationalem integram
         unius variabilis in factores reales primi vel secundi gradus resolvi posse</p>
