454ee2e6bb8d7ce898014bda4afa6e94dc3dc87d
Laith <62894505+lth17elm@users.noreply.github.com>
2020-11-19 12:43:52 +0100

    plugin removed

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'plugin:vue/vue3-recommended',
+    //'plugin:vue/vue3-recommended',
     '@vue/standard'
   ],
   parserOptions: {
