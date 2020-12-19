67ca000f7c83a8501d8626bb06116baa18840e52
Laith <62894505+lth17elm@users.noreply.github.com>
2020-12-19 23:16:56 +0100

    new icons

--- client/src/icon.js
+++ client/src/icon.js
@@ -1,10 +1,10 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faSortDown, faSignInAlt, faStar as fasFaStar } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faSortDown, faSignInAlt, faStar as fasFaStar, faUserPlus, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 // Utiliser uniquement les icônes archway, ad et brain
-library.add(faHome, faSortDown, faSignInAlt, fasFaStar, farFaStar, faGithub)
+library.add(faHome, faSortDown, faSignInAlt, fasFaStar, farFaStar, faGithub, faUserPlus, faCheckCircle)
 
 // Remplacer toutes les balises <i class="fa..."> par un <svg> et utiliser un MutationObserver pour
 // continuer à le faire quand le DOM change.
