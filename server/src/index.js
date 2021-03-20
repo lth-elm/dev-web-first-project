7ac84c35ded11a48b68ec08a430eedcb54a92b24
Laith <62894505+lth17elm@users.noreply.github.com>
2021-03-20 17:08:30 +0100

    add env port option

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ const router = require('./routes/index.js')
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.json())
