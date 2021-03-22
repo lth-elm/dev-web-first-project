9c7744f120120795624c5b8ae4a9b8a7a1b8eaae
Laith <62894505+lth17elm@users.noreply.github.com>
2021-03-22 19:19:09 +0100

    :wrench: get mongo atlas env variable

--- server/src/connect.js
+++ server/src/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
 
-// Connection URL
-const defaultMongoUrl = 'mongodb://venom:venom@localhost:27017/venom'
-
 const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -10,7 +7,7 @@ const mongoOptions = {
 
 module.exports = {
   getConnection () {
-    const mongoUrl = process.env.MONGO_URL || process.env.MONGO_ATLAS_URL || defaultMongoUrl
+    const mongoUrl = process.env.MONGO_ATLAS_URL || process.env.MONGO_URL
     return mongoose.connect(mongoUrl, mongoOptions)
   }
 }
