0699a0fcc8dcef937a9fae254ecc4e957b891cea
Laith <62894505+lth17elm@users.noreply.github.com>
2021-03-26 15:51:54 +0100

    :sparkles: add cross icon

--- client/src/icon.js
+++ client/src/icon.js
@@ -1,10 +1,10 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faSortDown, faSignInAlt, faSignOutAlt, faStar as fasFaStar, faUserPlus, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faSortDown, faSignInAlt, faSignOutAlt, faStar as fasFaStar, faUserPlus, faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 // Utiliser uniquement les icônes archway, ad et brain
-library.add(faHome, faSortDown, faSignInAlt, faSignOutAlt, fasFaStar, farFaStar, faGithub, faUserPlus, faCheckCircle)
+library.add(faHome, faSortDown, faSignInAlt, faSignOutAlt, fasFaStar, farFaStar, faGithub, faUserPlus, faCheckCircle, faTimesCircle)
 
 // Remplacer toutes les balises <i class="fa..."> par un <svg> et utiliser un MutationObserver pour
 // continuer à le faire quand le DOM change.
