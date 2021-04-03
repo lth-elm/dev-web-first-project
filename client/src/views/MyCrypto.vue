9e5a850926f8594c9f5d8f85d823140d11eda5c8
Laith <62894505+lth17elm@users.noreply.github.com>
2021-04-03 23:42:30 +0200

    :sparkles: Load Song for a City contract

--- client/src/views/MyCrypto.vue
+++ client/src/views/MyCrypto.vue
@@ -8,13 +8,18 @@
           <br>
           <div class="web3demo">
             <h2>Web 3 Demo</h2>
-            <button v-if="!address" v-on:click="loadWeb3();">Connect to Metamask</button>
+            <button v-if="!address" v-on:click="load();">Connection à Metamask</button>
             <p><b><i>Statut</i></b> &nbsp; : <span id="status">Connection requise...</span></p>
             <br>
-            <div v-if="address" class="connecte">
+            <div v-if="address">
                 <p>Adresse : {{ address }}</p>
                 <p>Chain Id : {{ chainId }}</p>
                 <p>Dernier bloc : {{ lastBlock }}</p>
+                <br>
+                <br>
+                <p>Nom du contrat : {{ contractName }} ({{ symbol }})</p>
+                <p>Supply totale : {{ totalSupply }}</p>
+                <button v-on:click="claimToken();">Réclamer un token</button>
             </div>
             <div v-if="noweb3">
                 <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank"><img src="https://www.french-ico.com/wp-content/uploads/2020/11/metamask-2-1.png" alt="Logo Metamask" /></a>
@@ -40,11 +45,36 @@ export default {
       web3: undefined,
       address: undefined,
       chainId: undefined,
-      lastBlock: undefined
+      lastBlock: undefined,
+      contract: undefined,
+      contractName: undefined,
+      symbol: undefined,
+      totalSupply: undefined
     }
   },
 
   methods: {
+    load: async function () {
+      await this.loadWeb3()
+      window.contract = await this.loadSongForCity()
+      this.contract = window.contract
+
+      this.contract.methods.name().call().then(e => {
+        console.log(e)
+        this.contractName = e
+      })
+
+      this.contract.methods.symbol().call().then(e => {
+        console.log(e)
+        this.symbol = e
+      })
+
+      this.contract.methods.totalSupply().call().then(e => {
+        console.log(e)
+        this.totalSupply = e
+      })
+    },
+
     loadWeb3: async function () {
       if (window.ethereum) {
         this.web3 = new Web3(window.ethereum)
@@ -75,10 +105,6 @@ export default {
       }
     },
 
-    // load: async function () {
-    //   this.loadWeb3()
-    // },
-
     updateStatus: function (status, connected) {
       const connectionStatus = document.getElementById('status')
       console.log(connectionStatus.innerHTML)
@@ -89,11 +115,444 @@ export default {
         connectionStatus.style.color = 'red'
       }
       console.log(status)
+    },
+
+    claimToken: function () {
+      this.contract.methods.claimAToken().call()
+    },
+
+    loadSongForCity: async function () {
+      return await new this.web3.eth.Contract([
+        {
+          inputs: [],
+          stateMutability: 'nonpayable',
+          type: 'constructor'
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'owner',
+              type: 'address'
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'approved',
+              type: 'address'
+            },
+            {
+              indexed: true,
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'Approval',
+          type: 'event'
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'owner',
+              type: 'address'
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'operator',
+              type: 'address'
+            },
+            {
+              indexed: false,
+              internalType: 'bool',
+              name: 'approved',
+              type: 'bool'
+            }
+          ],
+          name: 'ApprovalForAll',
+          type: 'event'
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'from',
+              type: 'address'
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'to',
+              type: 'address'
+            },
+            {
+              indexed: true,
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'Transfer',
+          type: 'event'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'to',
+              type: 'address'
+            },
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'approve',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'owner',
+              type: 'address'
+            }
+          ],
+          name: 'balanceOf',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'baseURI',
+          outputs: [
+            {
+              internalType: 'string',
+              name: '',
+              type: 'string'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'getApproved',
+          outputs: [
+            {
+              internalType: 'address',
+              name: '',
+              type: 'address'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'owner',
+              type: 'address'
+            },
+            {
+              internalType: 'address',
+              name: 'operator',
+              type: 'address'
+            }
+          ],
+          name: 'isApprovedForAll',
+          outputs: [
+            {
+              internalType: 'bool',
+              name: '',
+              type: 'bool'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'name',
+          outputs: [
+            {
+              internalType: 'string',
+              name: '',
+              type: 'string'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'ownerOf',
+          outputs: [
+            {
+              internalType: 'address',
+              name: '',
+              type: 'address'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'from',
+              type: 'address'
+            },
+            {
+              internalType: 'address',
+              name: 'to',
+              type: 'address'
+            },
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'safeTransferFrom',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'from',
+              type: 'address'
+            },
+            {
+              internalType: 'address',
+              name: 'to',
+              type: 'address'
+            },
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            },
+            {
+              internalType: 'bytes',
+              name: '_data',
+              type: 'bytes'
+            }
+          ],
+          name: 'safeTransferFrom',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'operator',
+              type: 'address'
+            },
+            {
+              internalType: 'bool',
+              name: 'approved',
+              type: 'bool'
+            }
+          ],
+          name: 'setApprovalForAll',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'bytes4',
+              name: 'interfaceId',
+              type: 'bytes4'
+            }
+          ],
+          name: 'supportsInterface',
+          outputs: [
+            {
+              internalType: 'bool',
+              name: '',
+              type: 'bool'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'symbol',
+          outputs: [
+            {
+              internalType: 'string',
+              name: '',
+              type: 'string'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'uint256',
+              name: 'index',
+              type: 'uint256'
+            }
+          ],
+          name: 'tokenByIndex',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'tokenCounter',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'owner',
+              type: 'address'
+            },
+            {
+              internalType: 'uint256',
+              name: 'index',
+              type: 'uint256'
+            }
+          ],
+          name: 'tokenOfOwnerByIndex',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'totalSupply',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: 'from',
+              type: 'address'
+            },
+            {
+              internalType: 'address',
+              name: 'to',
+              type: 'address'
+            },
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'transferFrom',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'uint256',
+              name: 'tokenId',
+              type: 'uint256'
+            }
+          ],
+          name: 'tokenURI',
+          outputs: [
+            {
+              internalType: 'string',
+              name: '',
+              type: 'string'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'claimAToken',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        }
+      ], '0x004a84209a0021b8ff182ffd8bb874c53f65e90e')
     }
   },
 
   mounted () {
-    this.loadWeb3()
+    this.load()
   },
 
   components: {
